1cae6d4620d69170f61af29994ce137b74d71f9c
Ian Oltuszyk <ioltuszy@ualberta.ca>
2019-04-07 16:27:25 -0600

    add third-party headers and libraries

--- Source/ue4_gpgpu_plugin/ue4_gpgpu_plugin.Build.cs
+++ Source/ue4_gpgpu_plugin/ue4_gpgpu_plugin.Build.cs
@@ -1,19 +1,25 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class ue4_gpgpu_plugin : ModuleRules
 {
+	private string ThirdPartyDirectory
+	{
+		get { return Path.Combine(ModuleDirectory, "..\\..\\ThirdParty"); }
+	}
+
 	public ue4_gpgpu_plugin(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
+				Path.Combine(ThirdPartyDirectory, "Include")
 				// ... add public include paths required here ...
 			}
 			);
-				
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
@@ -49,5 +55,21 @@ public class ue4_gpgpu_plugin : ModuleRules
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
+
+		string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
+		string OpenCLLibrariesPath = Path.Combine(ThirdPartyDirectory, "Lib");
+		string NvidiaLibrariesPath = Path.Combine(OpenCLLibrariesPath, "NVIDIA", PlatformString);
+		string IntelLibrariesPath = Path.Combine(OpenCLLibrariesPath, "Intel", PlatformString);
+		string AmdLibrariesPath = Path.Combine(OpenCLLibrariesPath, "AMD", PlatformString);
+		if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(NvidiaLibrariesPath, "OpenCL.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(IntelLibrariesPath, "OpenCL.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(AmdLibrariesPath, "OpenCL.lib"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalFrameworks.Add(new UEBuildFramework("OpenCL"));
+		}
 	}
 }
